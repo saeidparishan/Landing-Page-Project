2fdd997da62b2bde781438ae4f871adbd427359f
SAEID <saeidparishan@gmail.com>
2025-11-08 19:25:45 +0330

    .

--- core/settings/development.py
+++ core/settings/development.py
@@ -5,10 +5,25 @@ from .base import *
 DEBUG = True
 ALLOWED_HOSTS = ["*"]
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# } 
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('POSTGRES_DB'),
+        'USER': os.getenv('POSTGRES_USER'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
+        'HOST': os.getenv('POSTGRES_HOST'),
+        'PORT': os.getenv('POSTGRES_PORT', '5432'),
     }
 }
 INSTALLED_APPS += ['drf_yasg']
